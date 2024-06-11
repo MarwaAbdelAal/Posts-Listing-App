76893faae8275fd2d5a3251e0a34eee62f58ff86
MarwaAbdelAal <marwaabdelaal112@gmail.com>
2024-06-11 12:15:00 +0300

    Show/Hide navbar on scroll

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,12 +5,40 @@ import { GoHome, GoHomeFill } from "react-icons/go";
 import { MdFavorite, MdFavoriteBorder } from "react-icons/md";
 import styles from "@/styles/nav.module.scss";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
   const pathname = usePathname();
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  // Show/Hide navbar on scroll
+  const controlNavbar = () => {
+    if (typeof window !== "undefined") {
+      if (window.scrollY > lastScrollY) {
+        // if scroll down hide the navbar
+        setShowNavbar(false);
+      } else {
+        // if scroll up show the navbar
+        setShowNavbar(true);
+      }
+      setLastScrollY(window.scrollY);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", controlNavbar);
+
+      // cleanup function
+      return () => {
+        window.removeEventListener("scroll", controlNavbar);
+      };
+    }
+  });
 
   return (
-    <nav className={styles.nav}>
+    <nav className={`${styles.nav} ${showNavbar ? "" : styles.nav__hidden}`}>
       <Link
         href="/"
         className={`${styles.nav__item} ${
